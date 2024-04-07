91d1db1882ebf0092523f3920ce2e912b4322fba
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2024-04-07 22:22:17 +0530

    added application-lifeycle-mgmt

--- some-commands.sh
+++ some-commands.sh
@@ -1,8 +1,13 @@
-#to generate a yaml file
+#to generate a yaml file and other create commands
 kubectl create deployment --image=nginx nginx --replicas=4 --dry-run=client -o yaml > nginx-deployment.yaml
+kubectl create configmap webapp-config-map --from-literal=APP_COLOR=darkblue --from-literal=APP_OTHER=disregard 
 
 #to taint a node
 kubectl taint node node01 color=red:NoSchedule #add "-" at the end to remove the taint, respective tolerations should be added in podspec
 
 #to label the node
 kubectl label node node01 color=red
+
+#to update the image
+kubectl set image deployment/myapp container-name=repo/image:2.45
+
